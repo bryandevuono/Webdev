0f0ecbbb3783a927c2d427af2f93ebf5129e6d01
henri_078 <hj.vdstelt@gmail.com>
2024-09-24 11:09:07 +0200

    attendance.cs deleted

--- Models/Attendance.cs
+++
@@ -1,8 +0,0 @@
-public class OfficeAttendance
-{
-    public int? Id {get; set;}
-    public DateTime? Start { get; set; }
-    public DateTime? End { get; set; }
-    public Guid? UserId { get; set; }
-    public Guid? EventId { get; set; }
-}
