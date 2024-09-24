67d00ec06ea895946cfeb98b118caa79a5a7c676
henri_078 <hj.vdstelt@gmail.com>
2024-09-24 11:11:29 +0200

    OfficeAttendance created

---
+++ Models/OfficeAttendance.cs
@@ -0,0 +1,7 @@
+public class OfficeAttendance
+{
+    public Guid? OfficeAttendanceId { get; set; }
+    public DateTime? Start { get; set; }
+    public DateTime? End { get; set; }
+    public Guid? UserId { get; set; }
+}
