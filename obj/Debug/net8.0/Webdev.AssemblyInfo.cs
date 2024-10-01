e1a27d382c3eaa8ad38872be63e4574f98b3c2f5
Robin <robin.otterspeer2005@gmail.com>
2024-10-01 13:00:55 +0100

    name changes

--- obj/Debug/net8.0/Webdev.AssemblyInfo.cs
+++ obj/Debug/net8.0/Webdev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Webdev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5be3fcd74955b2c0b0de2a7979f77e8d2e964f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2440654c7733642c48a83570b5e84e9351fcdd96")]
 [assembly: System.Reflection.AssemblyProductAttribute("Webdev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Webdev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
