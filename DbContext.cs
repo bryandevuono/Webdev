7d747a2ed0a3b2effa29f24a0765a1a80335c4d8
henri_078 <hj.vdstelt@gmail.com>
2024-10-08 11:29:38 +0200

    OfficeAttendance werkt

--- DbContext.cs
+++ DbContext.cs
@@ -7,5 +7,5 @@ public class MyDbContext : DbContext
     public DbSet<Users> Users { get; set; }
     public DbSet<Events> Events { get; set; }
     public DbSet<Admin> Admin { get; set; }
-    public DbSet<OfficeAttendance> Attendance { get; set; }
+    public DbSet<OfficeAttendance> OfficeAttendance { get; set; }
 }
