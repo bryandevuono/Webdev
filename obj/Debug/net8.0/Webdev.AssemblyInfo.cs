00872e86fd2c5e62c1751b9ffba84792db561ac5
Robin <robin.otterspeer2005@gmail.com>
2024-10-08 13:39:01 +0100

    admin

--- obj/Debug/net8.0/Webdev.AssemblyInfo.cs
+++ obj/Debug/net8.0/Webdev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Webdev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06387f95802b07181d7dead511b30f2643ae16d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35e2e446e79035a18f3445d59bb6befbf7298a7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Webdev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Webdev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
