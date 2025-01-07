f4e54fabf1231907755e1116736a3f00de420175
Henri van der Stelt <hj.vdstelt@gmail.com>
2025-01-07 11:17:49 +0100

    logged in succesfully message displayed for admin and user

--- Frontend/my-app/src/components/LoginScreen.tsx
+++ Frontend/my-app/src/components/LoginScreen.tsx
@@ -1,60 +1,83 @@
 import React, { useState } from "react";
-import { checkIfLoggedIn, loginInput , postLogin} from "../api/Login"
+import { checkIfLoggedIn, loginInput, postLogin } from "../api/Login";
 import { useNavigate, Link } from "react-router-dom";
 
 interface LoginScreenProps {
-    setAuthorized: Function
+  setAuthorized: Function;
 }
-const LoginScreen = ({setAuthorized}: LoginScreenProps) : JSX.Element =>
-{
-    const Navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [errorMessage, setErrorMessage] = useState(false);
-    const [duplicateLogin, setDuplicateLogin] = useState(false);
+const LoginScreen = ({ setAuthorized }: LoginScreenProps): JSX.Element => {
+  const Navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [duplicateLogin, setDuplicateLogin] = useState(false);
+  const [loginCheck, setLoginCheck] = useState(false);
 
-    const handleLogin = async (email: string, password: string) => {
-        const userInfo: loginInput = {email: email, password: password};
-        const checkLogin = await postLogin(userInfo, Navigate);
-        if(checkLogin){
-            setAuthorized(true);
-            Navigate("/calendar");
-        }
-        else{
-            setErrorMessage(true);
-        }
+  const handleLogin = async (email: string, password: string) => {
+    const userInfo: loginInput = { email: email, password: password };
+    const checkLogin = await postLogin(userInfo, Navigate);
+    if (checkLogin) {
+      setLoginCheck(true);
+      setAuthorized(true);
+      setTimeout(() => {
+        Navigate("/calendar");
+      }, 1200);
+    } else {
+      setErrorMessage(true);
     }
+  };
 
-    const handleLoginClick = async ()=> {
-        const loginCheck = await checkIfLoggedIn()
-        if(loginCheck){
-            setDuplicateLogin(true);
-            return;
-        }
-        handleLogin(email, password)
+  const handleLoginClick = async () => {
+    const loginCheck = await checkIfLoggedIn();
+    if (loginCheck) {
+      setDuplicateLogin(true);
+      return;
     }
-    return(
-        <div className="flexbox">
-            <div className="login-box">
-                <label>
-                    Username:{" "}
-                    <input className="input-style" onChange={(event) => setEmail(event.target.value)}/>
-                </label>
-                <br/>
-                <label>
-                    Password:{" "}
-                    <input type="password" className="input-style" onChange={(event) => setPassword(event.target.value)}/>
-                </label>
-                <br/>
-                <button className="login-button" onClick={handleLoginClick}>Login</button>
-                <Link className="signup-button" to={'/signup'}><p>Sign up</p></Link>
-                <Link className="signup-button" to={'/adminlogin'}><p>Login as an admin</p></Link>
-                
-                {errorMessage ? <p className="error-text">Something went wrong...</p> : null}
-                {duplicateLogin ? <p className="error-text">Logged in already</p> : null}
-            </div>
-        </div>
-    );
-}
+    handleLogin(email, password);
+  };
+
+  return (
+    <div className="flexbox">
+      <div className="login-box">
+        <label>
+          Username:{" "}
+          <input
+            className="input-style"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:{" "}
+          <input
+            type="password"
+            className="input-style"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <br />
+        <button className="login-button" onClick={handleLoginClick}>
+          Login
+        </button>
+        <Link className="signup-button" to={"/signup"}>
+          <p>Sign up</p>
+        </Link>
+        <Link className="signup-button" to={"/adminlogin"}>
+          <p>Login as an admin</p>
+        </Link>
+
+        {loginCheck ? (
+          <p className="success-text">Logged in successfully!</p>
+        ) : null}
+        {errorMessage ? (
+          <p className="error-text">Something went wrong...</p>
+        ) : null}
+        {duplicateLogin ? (
+          <p className="error-text">Logged in already</p>
+        ) : null}
+      </div>
+    </div>
+  );
+};
 
 export default LoginScreen;
