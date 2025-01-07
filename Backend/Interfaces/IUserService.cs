53224e7cc7ae03bb1e332eae2b4b81b3d7564785
Robin <robin.otterspeer2005@gmail.com>
2025-01-07 11:19:32 +0100

    added ethode to get user by id and the office attendances are now displayed

--- Backend/Interfaces/IUserService.cs
+++ Backend/Interfaces/IUserService.cs
@@ -2,5 +2,6 @@ public interface IUserService
 {
     public Task<bool> AddUser(Users user);
     public Task<List<Users>> GetUser();
+    public Task<Users> GetUserById(Guid userId);
     public Task<bool> DeleteUser(Users user);
 }
