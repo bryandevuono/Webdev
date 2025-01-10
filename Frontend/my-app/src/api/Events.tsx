2bf36a1b1fbf62d15ffc6715420419e9d04203bd
bryandevuono <bryandevuono@gmail.com>
2025-01-10 14:12:45 +0100

    Added custom event type for events

--- Frontend/my-app/src/api/Events.tsx
+++ Frontend/my-app/src/api/Events.tsx
@@ -1,5 +1,9 @@
 import { Event } from "react-big-calendar"
 
+export type OfficeEvent = Event & {
+    eventId: string
+}
+
 export const getAllEvents = async (): Promise<Array<Event>> => {
     const response = await fetch('http://localhost:5053/api/events/GetAllEvents', {
         method: 'GET',
@@ -13,10 +17,11 @@ export const getAllEvents = async (): Promise<Array<Event>> => {
     const Events: Array<Event> = [];
 
     for (let i = 0; i < data.length; i++) {
-        const EventToAdd: Event = {
+        const EventToAdd: OfficeEvent = {
             start: new Date(data[i].startTime),
             end: new Date(data[i].endTime),
             title: data[i].title,
+            eventId: data[i].id
         };
         Events.push(EventToAdd);
     }
