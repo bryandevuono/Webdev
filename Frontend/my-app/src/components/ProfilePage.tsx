b43d493fe9514b7ac0e1655f0c0c0451d76fd374
bryandevuono <bryandevuono@gmail.com>
2025-01-10 17:30:27 +0100

    improved profile page and admin page

--- Frontend/my-app/src/components/ProfilePage.tsx
+++ Frontend/my-app/src/components/ProfilePage.tsx
@@ -1,25 +1,27 @@
 
 import {useEffect, useState} from "react";
-import { getUserInfo, logOut } from "../api/Login";
+import { getUserId, logOut } from "../api/Login";
+import {getUserData} from "../api/Users";
 import { Link } from "react-router-dom";
 import GetPoints from "../api/Ranking";
+import { checkAdmin } from "../api/Admin";
 
 interface ProfilePageProps {
   setAuthorized: Function;
 }
 
 const ProfilePage = ({ setAuthorized }: ProfilePageProps): JSX.Element => {
-  const [UserName, setUserName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [IsAdmin, setIsAdmin] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [Points, setPoints] = useState<number>(0);
 
-  const GetUserName = async () => {
-    try {
-      const UsernameFromAPI = await getUserInfo();
-      setUserName(UsernameFromAPI);
-      console.log(UserName);
-    } catch (error) {
-      console.error("Failed to fetch username:", error);
-    }
+  const GetProfile = async () => {
+    const userInfo = await getUserData(await getUserId());
+    setUserName(userInfo.email);
+    setFirstName(userInfo.firstName);
+    setLastName(userInfo.lastName);
   };
 
   const GetUserPoints = async () => {
@@ -36,18 +38,29 @@ const ProfilePage = ({ setAuthorized }: ProfilePageProps): JSX.Element => {
     logOut();
   };
 
-  useEffect(() => {
-    GetUserName();
-    GetUserPoints();
-  }, []);
+  const CheckIfUserIsAdmin = async () => {
+    setIsAdmin(await checkAdmin());
+  };
 
+  useEffect(() => {
+    GetProfile();
+    GetUserPoints();
+    CheckIfUserIsAdmin();
+  }, []);
   return (
     <div className="flexbox">
       <div className="card">
-        <h1>Hi, {UserName}!</h1>
-        <p>Points: {Points}</p>
+        <h1>Hi, {IsAdmin ? "Admin" : userName}!</h1>
+        {!IsAdmin && (
+          <>
+            <p>Name: {firstName}</p>
+            <p>Lastname: {lastName}</p>
+            <p>Username: {userName}</p>
+            <p>Points: {Points}</p>
+          </>
+        )}
         <Link to={"/"}>
-          <button className="login-button" onClick={() => LogOutAPI()}>
+          <button className="login-button" onClick={LogOutAPI}>
             Log out
           </button>
         </Link>
