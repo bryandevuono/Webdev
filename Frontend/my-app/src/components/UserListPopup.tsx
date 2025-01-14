9a38d31a90bc3d30c3b511c35a2c8d2a285188b7
Julina <Julinamay_@hotmail.com>
2025-01-14 11:42:50 +0100

    Show "no attendees yet" when there are none found

--- Frontend/my-app/src/components/UserListPopup.tsx
+++ Frontend/my-app/src/components/UserListPopup.tsx
@@ -14,12 +14,16 @@ const UserListPopup: React.FC<UserListPopupProps> = ({ isOpen, onClose, users, e
         <div className="popup-overlay">
             <div className="popup-form">
                 <h2>Attendees for {eventTitle}</h2>
-                <ul>
-                    {users.map((users, index) => (
-                        <li key={index}>{users}</li>
-                    ))}
-                </ul>
                 <button onClick={onClose}>Close</button>
+                {users.length === 0 ? (
+                    <p>No attendees yet</p>
+                ) : (
+                    <ul>
+                        {users.map((users, index) => (
+                            <li key={index}>{users}</li>
+                        ))}
+                    </ul>
+                )}
             </div>
         </div>
     );
